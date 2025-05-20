1de50a9f7f173147964cc9aa39ad1df9855578ea
carlos-henreis <carlos_henreis@yahoo.com.br>
2025-05-20 09:20:05 -0300

    Criando controller com as 3 rotas

---
+++ src/main/java/br/com/cahenre/encurtadorurl/adapter/in/web/UrlController.java
@@ -0,0 +1,45 @@
+package br.com.cahenre.encurtadorurl.adapter.in.web;
+
+import br.com.cahenre.encurtadorurl.adapter.in.web.dto.request.UrlEncurtadaRequest;
+import br.com.cahenre.encurtadorurl.adapter.in.web.dto.response.EstatisticasResponse;
+import br.com.cahenre.encurtadorurl.adapter.in.web.dto.response.UrlEncurtadaResponse;
+import br.com.cahenre.encurtadorurl.domain.port.in.UrlUseCase;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+public class UrlController {
+
+    @Autowired
+    private UrlUseCase urlUseCase;
+
+    @PostMapping
+    public ResponseEntity<UrlEncurtadaResponse> create(@RequestBody @Valid UrlEncurtadaRequest request) {
+        UrlEncurtadaResponse response = urlUseCase.createShortUrl(request.getUrlOrigem());
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<String> redirect(@PathVariable String shortCode) {
+        String originalUrl = urlUseCase.getOriginalUrl(shortCode);
+        return ResponseEntity.status(302).location(URI.create(originalUrl)).build();
+    }
+
+    @GetMapping("/stats/{shortCode}")
+    public ResponseEntity<EstatisticasResponse> getStatistics(@PathVariable String shortCode) {
+        return ResponseEntity.ok(urlUseCase.getStatistics(shortCode));
+    }
+
+    @GetMapping("/qrcode/{shortCode}")
+    public ResponseEntity<byte[]> getQrCode(@PathVariable String shortCode) throws Exception {
+        byte[] qrCode = urlUseCase.getQrCodeLink(shortCode);
+        return ResponseEntity.ok()
+                .header("Content-Type", "image/png")
+                .body(qrCode);
+    }
+
+}
