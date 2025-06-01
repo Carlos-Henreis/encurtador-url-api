6232bf03e6ed46633aac9990706a30c60008a6b0
carlos-henreis <carlos_henreis@yahoo.com.br>
2025-05-31 22:43:16 -0300

    Adicionando rate limit

--- src/main/java/br/com/cahenre/encurtadorurl/EncurtadorurlApplication.java
+++ src/main/java/br/com/cahenre/encurtadorurl/EncurtadorurlApplication.java
@@ -2,10 +2,12 @@ package br.com.cahenre.encurtadorurl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class EncurtadorurlApplication {
 
 	public static void main(String[] args) {
